ad5d71d3d509bc7fdf232279ede4d2a2df54c4ba
Mikkel Boese Johnsen <mikk571a@edu.campusvejle.dk>
2018-10-01 13:19:45 +0200

    Lavede Udlaan

---
+++ DanskBibliotekForening/Repository/Udlaan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class Udlaan
+    {
+        private DateTime afleveringsDato;
+
+        public DateTime AfleveringsDato
+        {
+            get { return afleveringsDato; }
+            set { afleveringsDato = value; }
+        }
+
+        public void BeregnAflereveringsDato(DateTime lenddate, string userStatus)
+        {
+            DateTime returnDate;
+            switch (userStatus.ToLower())
+            {
+                case "administrator":
+                    returnDate = lenddate.AddDays(120);
+                    break;
+                case "normallender":
+                    returnDate = lenddate.AddDays(30);
+                    break;
+                case "superlender":
+                    returnDate = lenddate.AddDays(60);
+                    break;
+                case "ansat":
+                    returnDate = lenddate.AddDays(40);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
